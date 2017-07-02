4890d9f08040eb32c6e78e305b56488301cc2722
Jonathan Abraham <jonathan.abraham@outlook.com>
2017-07-02 16:51:33 +0200

    setting socket.io and express base

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+
+server.listen(8080);
+
+console.log("server is listening on port 8080");
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+    socket.emit("tempreture", 50);
+});
