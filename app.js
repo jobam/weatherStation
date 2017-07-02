b4537e803c5e9215eaa48082bd2373dcc9f9991f
Jonathan Abraham <jonathan.abraham@outlook.com>
2017-07-02 20:46:19 +0200

    hightcharts integration

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app);  
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 app.use(express.static('public'));
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+    res.sendFile(__dirname + "/" + "index.html");
 })
 
 
