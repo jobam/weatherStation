681e660c9d1a8004f22b44b45c567a348fd5c402
Jonathan <jabraham@webnet.fr>
2017-07-20 13:46:30 +0200

    loading values at start and background color

--- app.js
+++ app.js
@@ -23,18 +23,23 @@ console.log("server is listening on port 8080");
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (client) {
     console.log('Un client est connecté !');
+    getData();
 });
 
 
 //================Get Data=================
 var t = setInterval(function () {
+    getData();
+}, 3000);
+
+function getData() {
 
     console.log("getting new Data");
     var result = sensor.getData();
     console.log("New data temp: " + result.temp + " hum: " + result.humidity);
 
     console.log("broadcasting result");
-        io.sockets.emit("tempreture", result.temp);
-        io.sockets.emit("humidity", result.humidity);
+    io.sockets.emit("tempreture", result.temp);
+    io.sockets.emit("humidity", result.humidity);
 
-}, 3000);
+}
